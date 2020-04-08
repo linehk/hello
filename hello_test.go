2c1d8c3d45054f883bb1cdbbd492ad49ae0e8ff3
linehk <sulinehk@gmail.com>
2020-04-08 15:08:27 +0800

    init

---
+++ hello_test.go
@@ -0,0 +1,11 @@
+package hello
+
+import (
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	if got, want := Hello(), "Hello, World!"; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
